6db937b8825f3f13d6c18bdf982aa88f588dd432
VoxWave <victor_bankowski@hotmail.com>
2016-05-27 12:32:50 +0300

    heal at well added

--- teams/cykablyat/heroes/valkyrie_main.lua
+++ teams/cykablyat/heroes/valkyrie_main.lua
@@ -133,8 +133,23 @@ end
 local healAtWellOldUtility = behaviorLib.HealAtWellBehavior["Utility"]
 
 local function HealAtWellUtilityOverride(botBrain)
-  if core.unitSelf:GetHealthPercent() and core.unitSelf:GetHealthPercent() < 0.15 then
-    return 999
+  if core.unitSelf:GetHealthPercent() and core.unitSelf:GetHealthPercent() < 0.50 then
+    local util = 1
+    local heroMul = 10
+    local pos = core.unitSelf:GetPosition()
+    local enemyHeroes = core.teamBotBrain.GetEnemyTeam(2200, range)
+    if enemyHeroes then
+      for _, _ in pairs(enemyHeroes) do
+          util = util * heroMul
+      end
+    end
+    local allyHeroes = core.teamBotBrain.GetAllyTeam(2200, range)
+    if allyHeroes then
+      for _, _ in pairs(allyHeroes) do
+          util = util / (heroMul * core.unitSelf:GetHealthPercent())
+      end
+    end
+    return util
   end
   return healAtWellOldUtility(botBrain)
 end
