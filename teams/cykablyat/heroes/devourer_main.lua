22f87115c063a35757958084a7f2d5480012b721
Victor Bankowski <victor_bankowski@hotmail.com>
2016-05-27 09:25:42 +0300

    cykablyat

--- teams/cykablyat/heroes/devourer_main.lua
+++ teams/cykablyat/heroes/devourer_main.lua
@@ -157,14 +157,14 @@ local healAtWellOldUtility = behaviorLib.HealAtWellBehavior["Utility"]
 local function HealAtWellUtilityOverride(botBrain)
   if core.unitSelf:GetHealthPercent() and core.unitSelf:GetHealthPercent() < 0.15 then
     local util = 0
-    local heroAddition = 20
+    local heroMul = 10
     local range = 2200
     local rangeSq = range * range
     local enemyHeroes = core.CopyTable(core.localUnits["EnemyHeroes"]);
     if enemyHeroes then
       for _, enemy in pairs(enemyHeroes) do
         if Vector3.Distance2DSq(enemy:GetPosition(), myPos) < rangeSq then
-          util = util + heroAddition
+          util = util * heroMul
         end
       end
     end
